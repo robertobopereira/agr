e0e22b2f9eec27d34c21e2557d36836374f54835
aluno <robertobopereira@gmail.com>
2023-09-28 16:47:42 -0400

    Novo Samba

--- samba/script.sh
+++ samba/script.sh
@@ -12,6 +12,16 @@ while IFS=',' read -r a b; do
 done < dados.csv
 
 echo "Ajustando permissao dos diretórios"
+
+lista=$(grep "path" /etc/samba/smb.conf)
+
+echo "$lista" | while IFS= read -r linha; do
+    valor=$(echo "$linha" | grep -oP 'path\s*=\s*\K[^[:space:]]+')
+    if [ ! -d "$valor" ]; then
+       echo "Criando diretório: $valor"
+       mkdir -p $valor
+    fi
+done
 chmod 777 /dados -R
 
 # Pegando o IP e mostrando
